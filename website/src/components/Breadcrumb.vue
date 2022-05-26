39e2ef3518de3d50486f2e3b9e5fa90ac2fae0fd
Lucas Carvalho <lucashms.carvalho@gmail.com>
2022-05-26 19:09:50 -0300

    feat: Breadcrumb for Desktop

--- website/src/components/Breadcrumb.vue
+++ website/src/components/Breadcrumb.vue
@@ -1,15 +1,42 @@
 <template>
-  <section class="breadcrumb" role="navigation">
+  <section 
+    class="breadcrumb" 
+    role="navigation"
+    aria-label="Indicação da navegação do site (breadcrumb)"
+    v-if='layoutMobile'
+  >
     <i class="fa-solid fa-angle-left"></i>
     <span class="breadcrumb__text">
       Minha conta
+    </span>
+  </section>
+
+  <section 
+    class="breadcrumb-g" 
+    role="navigation"
+    aria-label="Indicação da navegação do site (breadcrumb)"
+    v-else
+  >
+    <span class="breadcrumb-g__text">
+      Home
+    </span>
+    <span class="breadcrumb-g__text">
+      Minha conta
+    </span>
+    <span class="breadcrumb-g__text breadcrumb-g__text--active">
+      Bolsas favoritas
     </span>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Breadcrumb'
+  name: 'Breadcrumb',
+  computed: {
+    layoutMobile() {
+      return window.innerWidth < 720;
+    },
+  },
 }
 </script>
 
@@ -21,6 +48,27 @@ export default {
   &__text {
     font-weight: 500;
     margin-left: 0.5rem;
+  }
+}
+
+.breadcrumb-g {
+  padding: 1rem 1.5rem;
+  color: var(--blue-secondary);
+  max-width: 1240px;
+  margin: 0 auto;
+
+  &__text {
+    font-weight: 500;
+
+    & + &:before {  
+      content: '/';
+      display: inline-block;
+      margin: 0 1rem;
+    }
+
+    &--active {
+      color: var(--black);
+    }
   }
 }
 </style>
